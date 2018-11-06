d98e1ed643a219a990d6e42c3e275e010261d6e2
Simen Fonnes <simen@beiningbogen.no>
2018-11-06 13:20:32 +0100

    add input to test template

--- File Templates/Silver Platter/ViewModelTest.xctemplate/___FILEBASENAME___.swift
+++ File Templates/Silver Platter/ViewModelTest.xctemplate/___FILEBASENAME___.swift
@@ -7,27 +7,26 @@
 //
 
 import XCTest
+import ReactiveCocoa
+import ReactiveSwift
 
-class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_testSubclass___ {
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+
+    let viewModel = ___VARIABLE_viewModel___()
+
+    let test = TestObserver<String, NoError>()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        viewModel.outputs.test.observe(test.observer)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
-
 }
