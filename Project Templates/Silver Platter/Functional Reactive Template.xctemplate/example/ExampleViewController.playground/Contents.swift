9116bc860a8a70cf22ff63f4275e19e30488c726
Simen Fonnes <simen@beiningbogen.no>
2018-11-20 20:26:03 +0100

    remove project templates

--- Project Templates/Silver Platter/Functional Reactive Template.xctemplate/example/ExampleViewController.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import ___VARIABLE_productName___Api
-import PlaygroundSupport
-import ___VARIABLE_productName___Framework
-
-let exampleViewController = ExampleViewController.init(nibName: nil, bundle: nil)
-let navController = UINavigationController.init(rootViewController: exampleViewController)
-
-PlaygroundPage.current.liveView = navController
